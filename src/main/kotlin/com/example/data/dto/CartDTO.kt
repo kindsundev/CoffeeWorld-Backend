2f67afe5ca07d85d6cce0b81c9a6f8f4a9d53e56
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-03 21:23:40 +0700

    delete some dto class when not in use

--- src/main/kotlin/com/example/data/dto/CartDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class CartDTO(
-    val drinksId: Int,
-    val quantity: Int
-)
--- src/main/kotlin/com/example/data/dto/DrinksDTO.kt
+++
@@ -1,9 +0,0 @@
-package com.example.data.dto
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class DrinksDTO(
-    val quantity: Int,
-    val rating: Float? = null
-)

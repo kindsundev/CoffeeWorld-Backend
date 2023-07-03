15b5299b739a68695074b1fb37ca572930f85bc4
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-03 11:46:49 +0700

    initialized cafe routing in app and update app test class

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,9 +1,12 @@
 package com.example
 
+import com.example.controller.CafeController
+import com.example.data.db.DatabaseConnector
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
+import com.example.repository.CafeRepository
 
 fun main() {
     embeddedServer(Netty, port = 9999, host = "0.0.0.0", module = Application::module)
@@ -13,4 +16,11 @@ fun main() {
 fun Application.module() {
     configureSerialization()
     configureRouting()
+    initializedCafeRouting()
+}
+
+private fun Application.initializedCafeRouting() {
+    val cafeRepository = CafeRepository(DatabaseConnector)
+    val cafeController = CafeController(cafeRepository)
+    configureCafeRouting(cafeController)
 }

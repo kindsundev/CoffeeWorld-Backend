73260724c0f3a095e0a9835f09684295e79dbdb8
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-03 11:40:32 +0700

    change field name

--- src/main/kotlin/com/example/data/db/DatabaseConnector.kt
+++ src/main/kotlin/com/example/data/db/DatabaseConnector.kt
@@ -3,7 +3,7 @@ package com.example.data.db
 import org.ktorm.database.Database
 
 object DatabaseConnector {
-    val database = Database.connect(
+    val connect = Database.connect(
         url = "jdbc:mysql://localhost:3306/coffee_world",
         driver = "com.mysql.cj.jdbc.Driver",
         user = "root",

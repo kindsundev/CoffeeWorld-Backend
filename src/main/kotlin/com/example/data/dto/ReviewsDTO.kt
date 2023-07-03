d303e27215b04f43e4a9d52b3130bd11aa3683cd
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-03 21:15:15 +0700

    update label to property in model class and dto class

--- src/main/kotlin/com/example/data/dto/ReviewsDTO.kt
+++ src/main/kotlin/com/example/data/dto/ReviewsDTO.kt
@@ -1,9 +1,14 @@
 package com.example.data.dto
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReviewsDTO(
+    @SerialName("drinks_id")
+    val drinksId: Int,
+    @SerialName("user_id")
+    val userId: Int,
     val rating: Float,
     val comment: String? = null
 )
--- src/main/kotlin/com/example/data/model/ReviewsModel.kt
+++ src/main/kotlin/com/example/data/model/ReviewsModel.kt
@@ -1,11 +1,14 @@
 package com.example.data.model
 
+import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class ReviewsModel(
     val id: Int,
+    @SerialName("drinks_id")
     val drinksId: Int,
+    @SerialName("user_id")
     val userId: Int,
     val rating: Float,
     val comment: String? = null

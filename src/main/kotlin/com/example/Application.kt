7914f0bdb90792d70da2979abcb7d6148b9b6632
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-03 17:11:03 +0700

    add application util for init routing

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,18 +1,12 @@
 package com.example
 
-import com.example.controller.CafeController
-import com.example.controller.CategoryController
-import com.example.controller.DrinksController
-import com.example.data.db.DatabaseConnector
 import io.ktor.server.application.*
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import com.example.plugins.*
-import com.example.repository.CafeRepository
-import com.example.repository.CategoryRepository
-import com.example.repository.DrinksRepository
-
-private val database = DatabaseConnector.connect
+import com.example.util.*
+import com.example.util.initializedCategoryRouting
+import com.example.util.initializedDrinksRouting
 
 fun main() {
     embeddedServer(Netty, port = 9999, host = "0.0.0.0", module = Application::module)
@@ -25,22 +19,4 @@ fun Application.module() {
     initializedCafeRouting()
     initializedDrinksRouting()
     initializedCategoryRouting()
-}
-
-private fun Application.initializedCafeRouting() {
-    val cafeRepository = CafeRepository(database)
-    val cafeController = CafeController(cafeRepository)
-    configureCafeRouting(cafeController)
-}
-
-private fun Application.initializedDrinksRouting() {
-    val drinksRepository = DrinksRepository(database)
-    val drinksController = DrinksController(drinksRepository)
-    configureDrinksRouting(drinksController)
-}
-
-private fun Application.initializedCategoryRouting() {
-    val categoryRepository = CategoryRepository(database)
-    val categoryController = CategoryController(categoryRepository)
-    configureCategoryRouting(categoryController)
 }
---
+++ src/main/kotlin/com/example/util/ApplicationUtil.kt
@@ -0,0 +1,32 @@
+package com.example.util
+
+import com.example.controller.CafeController
+import com.example.controller.CategoryController
+import com.example.controller.DrinksController
+import com.example.data.db.DatabaseConnector
+import com.example.plugins.configureCafeRouting
+import com.example.plugins.configureCategoryRouting
+import com.example.plugins.configureDrinksRouting
+import com.example.repository.CafeRepository
+import com.example.repository.CategoryRepository
+import com.example.repository.DrinksRepository
+import io.ktor.server.application.*
+
+private val database = DatabaseConnector.connect
+fun Application.initializedCafeRouting() {
+    val cafeRepository = CafeRepository(database)
+    val cafeController = CafeController(cafeRepository)
+    configureCafeRouting(cafeController)
+}
+
+fun Application.initializedDrinksRouting() {
+    val drinksRepository = DrinksRepository(database)
+    val drinksController = DrinksController(drinksRepository)
+    configureDrinksRouting(drinksController)
+}
+
+fun Application.initializedCategoryRouting() {
+    val categoryRepository = CategoryRepository(database)
+    val categoryController = CategoryController(categoryRepository)
+    configureCategoryRouting(categoryController)
+}

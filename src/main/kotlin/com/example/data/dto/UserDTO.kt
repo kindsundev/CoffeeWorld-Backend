6398793ffa1fa054974785384df265850ba73490
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-06 15:19:49 +0700

    add validate function and hash password function

--- src/main/kotlin/com/example/data/dto/UserDTO.kt
+++ src/main/kotlin/com/example/data/dto/UserDTO.kt
@@ -1,14 +1,25 @@
 package com.example.data.dto
 
 import kotlinx.serialization.Serializable
+import org.mindrot.jbcrypt.BCrypt
 
 @Serializable
 data class UserDTO(
-    val userName: String,
+    val username: String,
     val password: String,
     val image: String? = null,
     val email: String,
     val name: String,
     val address: String? = null,
     val phone: String? = null
-)
+) {
+
+    // the main validation will be on the client (mobile app)
+    fun isValidCredentials(): Boolean {
+        return (username.length > 4) && (password.length >= 8) && (email.isNotEmpty()) && (name.isNotEmpty())
+    }
+
+    fun hashedPassword(): String {
+        return BCrypt.hashpw(password, BCrypt.gensalt())
+    }
+}

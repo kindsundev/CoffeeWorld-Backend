a2db10f2d26ed78c31dae3eb61a01b90ca954578
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-09 14:15:40 +0700

    add dependency java mail

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,4 +34,5 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
+    implementation("com.sun.mail:javax.mail:1.6.2")
 }

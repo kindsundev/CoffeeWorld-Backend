d022a93445d9f908156f70926cbed853874c4d84
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-10 14:47:01 +0700

    add dependency commons lang3 and commons email

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,5 +34,6 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
-    implementation("com.sun.mail:javax.mail:1.6.2")
+    implementation("org.apache.commons:commons-email:1.5")
+    implementation("org.apache.commons:commons-lang3:3.12.0")
 }

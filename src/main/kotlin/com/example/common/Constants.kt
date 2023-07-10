6ed4fd0a3f2cea1f81ae69bdc90da51175d81614
kind sun dev <mr.duongtiendev@gmail.com>
2023-07-10 14:47:25 +0700

    add Constants class

--- src/main/kotlin/com/example/common/Constants.kt
+++ src/main/kotlin/com/example/common/Constants.kt
@@ -6,4 +6,7 @@ object Constants {
     const val INVALID_USER_DATA = "invalid_user_data"
     const val REGISTER_FAILED = "register_failed"
     const val REGISTER_SUCCESS = "register_success"
+    const val SEND_EMAIL_SUCCESS = "send_email_success"
+    const val SEND_EMAIL_FAILED = "send_email_failed"
+    const val SQL_ERROR = "sql_error"
 }
